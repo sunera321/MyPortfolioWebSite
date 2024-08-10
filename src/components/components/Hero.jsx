0b220938d62f9ffecca715efb01779bc13ab039c
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-08-10 12:00:01 +0530

    Fix link and add alert to prevent accidental navigation in Hero component

--- src/components/components/Hero.jsx
+++ src/components/components/Hero.jsx
@@ -41,9 +41,13 @@ const Hero = () => {
             <div
               class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
             </div>
-            <a href="#contact" title="Get quote now"
+            <a href="#" title="Get quote now"
+              //on click alart 
+              onClick={() => {
+                alert("temparaly disabled")
+              }}
               class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-              role="button">Hire Me
+              role="button">Download CV
             </a>
           </div>
         </div>
